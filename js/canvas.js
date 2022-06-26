d225b829ea246b0ccb19188f4a52bf8f490d7bcf
NivEz <niv.cyber@gmail.com>
2022-06-27 02:09:02 +0300

    code cleanup + prettier

--- js/canvas.js
+++ js/canvas.js
@@ -1,24 +1,25 @@
-import {getRandomIntInclusive} from './utils.js'
-import state from './state.js';
+import { getRandomIntInclusive } from "./utils.js";
+import state from "./state.js";
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 400;
-canvas.height = 400;
+const WIDTH = 400;
+const HEIGHT = 400;
+
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
 
 function Line({
-                  width = canvas.width,
-                  radius = canvas.width / 2,
-                  speed = 1,
-                  isSingleLine = false,
-                  randomSpawn = false,
-                  changeRadiusUnit = 0,
-                  lineDash = [],
-                  baseColor = "black",
-                  secondColor = "",
-              } = {}) {
-    this.width = width;
+    radius = canvas.width / 2,
+    speed = 1,
+    isSingleLine = false,
+    randomSpawn = false,
+    changeRadiusUnit = 0,
+    lineDash = [],
+    baseColor = "black",
+    secondColor = "",
+} = {}) {
     this.radius = radius;
     this.speed = speed;
     this.isSingleLine = isSingleLine;
@@ -31,52 +32,52 @@ function Line({
     // local attributes
     this.angle = 1;
     this.direction = 1;
-    this.maxRadius = Math.sqrt(2 * (this.width ** 2)) / 2
+    this.maxRadius = Math.sqrt(2 * WIDTH ** 2) / 2;
     this.initialRadius = 200;
 
     this.update = () => {
-        this.setAngleAndSpeed()
-        this.setRadius()
+        this.setAngleAndSpeed();
+        this.setRadius();
         ctx.setLineDash(this.lineDash);
-    }
+    };
 
     this.draw = () => {
         if (this.isSingleLine) {
             this.destroy();
         }
-        ctx.save()
-        ctx.translate(this.width / 2, this.width / 2)
-        ctx.rotate(this.angle * Math.PI / 180)
-        ctx.beginPath()
-        ctx.moveTo(0, 0)
-        ctx.lineTo(this.radius, 0)
+        ctx.save();
+        ctx.translate(WIDTH / 2, HEIGHT / 2);
+        ctx.rotate((this.angle * Math.PI) / 180);
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.lineTo(this.radius, 0);
 
-        this.handleColors()
+        this.handleColors();
 
-        ctx.stroke()
-        ctx.restore()
-    }
+        ctx.stroke();
+        ctx.restore();
+    };
 
     this.setAngleAndSpeed = () => {
         if (this.randomSpawn) {
-            this.angle = getRandomIntInclusive(0, 360)
+            this.angle = getRandomIntInclusive(0, 360);
         }
         this.angle = (this.angle + this.speed) % 360;
-    }
+    };
 
     this.setRadius = () => {
         if (this.radius >= this.initialRadius && this.direction !== -1) {
             // make smaller
             this.radius = this.initialRadius;
-            this.direction = -1
+            this.direction = -1;
         } else if (this.radius <= 0 && this.direction !== 1) {
             // make bigger
             this.radius = 0;
-            this.direction = 1
+            this.direction = 1;
         } else {
-            this.radius += this.changeRadiusUnit * this.direction
+            this.radius += this.changeRadiusUnit * this.direction;
         }
-    }
+    };
 
     this.handleColors = () => {
         if (this.secondColor) {
@@ -87,11 +88,11 @@ function Line({
         } else {
             ctx.strokeStyle = this.baseColor;
         }
-    }
+    };
 
     this.destroy = () => {
-        ctx.clearRect(0, 0, this.width, this.width);
-    }
+        ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    };
 }
 
 export const line = new Line({
@@ -100,17 +101,19 @@ export const line = new Line({
     isSingleLine: false,
     randomSpawn: false,
     changeRadiusUnit: 0,
-    lineDash: []
-})
+    lineDash: [],
+    baseColor: "black",
+    secondColor: "",
+});
 
 const animate = () => {
     if (state.stop === false) {
-        line.update()
-        line.draw()
+        line.update();
+        line.draw();
     }
-    requestAnimationFrame(animate)
-}
+    requestAnimationFrame(animate);
+};
 
-addEventListener('load', () => {
-    requestAnimationFrame(animate)
-})
+addEventListener("load", () => {
+    requestAnimationFrame(animate);
+});
--- js/form.js
+++ js/form.js
@@ -1,69 +1,64 @@
-import {line} from './canvas.js'
+import { line } from "./canvas.js";
 import state from "./state.js";
 
 // ----- radius -----
-const handleRadius = e => {
-    line.radius = Number(e.target.value)
-}
+const handleRadius = (e) => {
+    line.radius = Number(e.target.value);
+};
 
 const radiusSlider = document.getElementById("radius");
-radiusSlider.oninput = handleRadius
+radiusSlider.oninput = handleRadius;
 // set max radius size
 radiusSlider.max = line.maxRadius;
 
-
 // ----- speed -----
-const handleSpeed = e => {
+const handleSpeed = (e) => {
     let val = Number(e.target.value);
     if (!val) {
         val = 1;
     }
-    line.speed = val
-}
+    line.speed = val;
+};
 
 const speedInput = document.getElementById("speed");
-speedInput.oninput = handleSpeed
-
+speedInput.oninput = handleSpeed;
 
 // ----- single line -----
-const handleIsSingleLine = e => {
+const handleIsSingleLine = (e) => {
     line.isSingleLine = e.target.checked;
-}
+};
 
 const singeLineCheckBox = document.getElementById("single-line");
-singeLineCheckBox.oninput = handleIsSingleLine
-
+singeLineCheckBox.oninput = handleIsSingleLine;
 
 // ----- random spawn -----
-const handleRandomSpawn = e => {
+const handleRandomSpawn = (e) => {
     line.randomSpawn = e.target.checked;
-}
+};
 
-const randomSpawnCheckbox = document.getElementById("random-spawn")
+const randomSpawnCheckbox = document.getElementById("random-spawn");
 randomSpawnCheckbox.oninput = handleRandomSpawn;
 
-
 // ----- radius change -----
-const handleRadiusChange = e => {
+const handleRadiusChange = (e) => {
     let val = Number(e.target.value);
     if (!val) {
         val = 0;
     }
     line.changeRadiusUnit = val;
-}
+};
 
 const radiusSizeChangeSlider = document.getElementById("radius-size-change");
 radiusSizeChangeSlider.oninput = handleRadiusChange;
 
-
 // ----- colors -----
 const baseColorPicker = document.getElementById("base-color");
-baseColorPicker.oninput = e => line.baseColor = e.target.value;
+baseColorPicker.oninput = (e) => (line.baseColor = e.target.value);
 
 const secondColorPicker = document.getElementById("second-color");
 const secondColorCheckbox = document.getElementById("second-color-checkbox");
 
-secondColorCheckbox.oninput = e => {
+secondColorCheckbox.oninput = (e) => {
     const isChecked = e.target.checked;
     // toggle - enable or disable checkbox
     secondColorPicker.disabled = !isChecked;
@@ -72,15 +67,14 @@ secondColorCheckbox.oninput = e => {
     } else {
         line.secondColor = "";
     }
-}
+};
 
-secondColorPicker.oninput = e => {
+secondColorPicker.oninput = (e) => {
     line.secondColor = e.target.value;
-}
-
+};
 
 // ----- line dash -----
-const lineDashHandler = e => {
+const lineDashHandler = (e) => {
     let val = e.target.value;
     let lineDashResult;
     if (!val) {
@@ -89,17 +83,16 @@ const lineDashHandler = e => {
         return;
     }
     const splittedArr = val.split("-");
-    const isValid = splittedArr.every(strItem => !isNaN(Number(strItem)));
+    const isValid = splittedArr.every((strItem) => !isNaN(Number(strItem)));
     if (!isValid) {
         return;
     }
-    lineDashResult = splittedArr.map(strNum => Number(strNum));
+    lineDashResult = splittedArr.map((strNum) => Number(strNum));
     line.lineDash = lineDashResult;
-}
+};
 
 const lineDashInput = document.getElementById("line-dash");
 lineDashInput.onchange = lineDashHandler;
-
 
 // ----- buttons -----
 const cleanButton = document.getElementById("clean-btn");
@@ -109,10 +102,10 @@ const stopButton = document.getElementById("stop-btn");
 stopButton.onclick = () => {
     state.stop = !state.stop;
     stopButton.innerText = state.stop ? "Play" : "Pause";
-}
+};
 
 const resetButton = document.getElementById("reset-btn");
 resetButton.onclick = () => {
     location.reload();
     line.destroy();
-}
+};
